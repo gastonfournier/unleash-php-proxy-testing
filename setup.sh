bef43b3a78f1043b84b3f0e5e28b233efd474342
Simon Hornby <liquidwicked64@gmail.com>
2024-04-22 12:52:21 +0200

    make it 10000 times

--- setup.sh
+++ setup.sh
@@ -1,24 +1,25 @@
 set -e
 
-# create a feature toggle
-wget --header 'Authorization: *:*.unleash-insecure-admin-api-token' \
-     --header 'Content-Type: application/json' \
-     --post-data='{
-  "type": "release",
-  "name": "a_toggle",
-  "description": "",
-  "impressionData": false
-}' \
-     --no-check-certificate \
-     --output-document - \
-     'http://unleash:4242/api/admin/projects/default/features'
+for i in {1..10000}; do
+  wget --header 'Authorization: *:*.unleash-insecure-admin-api-token' \
+      --header 'Content-Type: application/json' \
+      --post-data='{
+    "type": "release",
+    "name": "a_toggle_'$i'",
+    "description": "",
+    "impressionData": false
+  }' \
+      --no-check-certificate \
+      --output-document - \
+      'http://unleash:4242/api/admin/projects/default/features'
 
-# enable the feature toggle for the development environment
-wget --header 'Authorization: *:*.unleash-insecure-admin-api-token' \
-     --no-check-certificate \
-     --output-document - \
-     --post-data='' \
-     'http://unleash:4242/api/admin/projects/default/features/a_toggle/environments/development/on'
+  # enable the feature toggle for the development environment
+  wget --header 'Authorization: *:*.unleash-insecure-admin-api-token' \
+      --no-check-certificate \
+      --output-document - \
+      --post-data='' \
+      'http://unleash:4242/api/admin/projects/default/features/a_toggle_'$i'/environments/development/on'
+done
 
 
 # create a frontent api key and store it in /token/frontend.txt so php SDK can read it
